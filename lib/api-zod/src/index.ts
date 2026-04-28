948e330d777368fa64c86301dfa184511e87db91
ij5009518 <ij5009518@gmail.com>
2026-04-27 23:33:14 -0400

    Repair broken api-server syntax after bad merge

--- lib/api-zod/src/index.ts
+++ lib/api-zod/src/index.ts
@@ -1,2 +1,28 @@
 export * from "./generated/api";
-export * from "./generated/types";
+
+export * from "./generated/types/adminStats";
+export * from "./generated/types/alertSettings";
+export * from "./generated/types/errorResponse";
+export * from "./generated/types/getSmsLogsParams";
+export * from "./generated/types/healthStatus";
+export * from "./generated/types/linkAccountRequest";
+export * from "./generated/types/linkAccountRequestAccountType";
+export * from "./generated/types/linkedAccount";
+export * from "./generated/types/registerUserRequest";
+export * from "./generated/types/simulateSmsRequest";
+export * from "./generated/types/smsLog";
+export * from "./generated/types/smsLogDirection";
+export * from "./generated/types/smsLogStatus";
+export * from "./generated/types/tellerAccountSummary";
+export * from "./generated/types/tellerConfig";
+export * from "./generated/types/tellerConfigEnvironment";
+export * from "./generated/types/tellerEnrollRequest";
+export * from "./generated/types/transaction";
+export * from "./generated/types/transactionType";
+export * from "./generated/types/user";
+export * from "./generated/types/userOnboardingStatus";
+export * from "./generated/types/userWithAccounts";
+
+export type { GetUserTransactionsParams } from "./generated/types/getUserTransactionsParams";
+export type { SimulateSmsResponse } from "./generated/types/simulateSmsResponse";
+export type { TellerEnrollResponse } from "./generated/types/tellerEnrollResponse";
